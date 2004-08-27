905cdf3e5adadaad136adadd48022a90fd3c6ad9
obarthel <obarthel>
2004-08-27 13:49:17 +0000

    - Replaced the rint() and rintf() functions with the fdlibm code.

--- math_headers.h
+++ math_headers.h
@@ -148,6 +148,33 @@ do {												\
   (d) = sl_u.value;									\
 } while (0)
 
+/* A union which permits us to convert between a float and a 32 bit
+   int. */
+
+typedef union
+{
+  float value;
+  unsigned int word;
+} ieee_float_shape_type;
+
+/* Get a 32 bit int from a float.  */
+
+#define GET_FLOAT_WORD(i,d)		\
+do {							\
+  ieee_float_shape_type gf_u;	\
+  gf_u.value = (d);				\
+  (i) = gf_u.word;				\
+} while (0)
+
+/* Set a float from a 32 bit int.  */
+
+#define SET_FLOAT_WORD(d,i)		\
+do {							\
+  ieee_float_shape_type sf_u;	\
+  sf_u.word = (i);				\
+  (d) = sf_u.value;				\
+} while (0)
+
 extern double __kernel_cos(double x, double y);
 extern double __kernel_sin(double x, double y, int iy);
 extern int __rem_pio2(double x, double *y);

35a76cf5a20b999a9daeaad190b419276c1e5722
obarthel <obarthel>
2005-05-29 14:45:28 +0000

    - Implemented fmin()/fminf(), fmax()/fmaxf(), fdim()/fdimf() for C99.
    - Ported acosf(), asinf(), atan2f(), atanf(), ceilf(), expf(), floorf(),
      fmodf(), frexpf(), ldexpf(), log10f(), logbf(), logf(), modff(), powf(), sqrtf(),
      scalbn() and scalbnf() for C99.
    
    - Ported cbrt(), cbrtf(), erf(), erff(), erfc(), erfcf(), expm1(),
      expm1f(), ilogb(), ilogbf(), log1p() and log1pf() for C99.

--- math_ldexpf.c
+++ math_ldexpf.c
@@ -44,8 +44,21 @@
 float
 ldexpf(float x,int exp)
 {
-	/* ZZZ unimplemented */
-	return(0);
+	float result;
+
+	if(isinf(x) || fpclassify(x) == FP_ZERO)
+	{
+		result = x;
+	}
+	else
+	{
+		result = scalbnf(x,exp);
+
+		if(isinf(result) || (result < FLT_MIN || result > -FLT_MIN))
+			__set_errno(ERANGE);
+	}
+
+	return(result);
 }
 
 /****************************************************************************/

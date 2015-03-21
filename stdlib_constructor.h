839ba97f857ddc6b3569eff69232c60725b58125
Sebastian Bauer <mail@sebastianbauer.info>
2015-03-21 07:52:49 +0100

    Gcc had problems with the macros. Fixed it by appending a '_' to the names.

--- stdlib_constructor.h
+++ stdlib_constructor.h
@@ -81,16 +81,16 @@
 #else
 
 #define CONSTRUCTOR(name,pri) \
-	asm(".stabs \"___INIT_LIST__\",22,0,0,___ctor_" #name); \
+	asm(".stabs \"___INIT_LIST__\",22,0,0,___ctor_" #name "_"); \
 	asm(".stabs \"___INIT_LIST__\",20,0,0," #pri); \
-	VOID __ctor_##name##(VOID); \
-	VOID __ctor_##name##(VOID)
+	VOID __ctor_##name##_(VOID); \
+	VOID __ctor_##name##_(VOID)
 
 #define DESTRUCTOR(name,pri) \
-	asm(".stabs \"___EXIT_LIST__\",22,0,0,___dtor_" #name); \
+	asm(".stabs \"___EXIT_LIST__\",22,0,0,___dtor_" #name "_"); \
 	asm(".stabs \"___EXIT_LIST__\",20,0,0," #pri); \
-	VOID __dtor_##name##(VOID); \
-	VOID __dtor_##name##(VOID)
+	VOID __dtor_##name##_(VOID); \
+	VOID __dtor_##name##_(VOID)
 
 #endif /* __amigaos4__ */
 
